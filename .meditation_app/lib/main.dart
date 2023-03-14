8ce8d23033d8534c6c2169cee4d66b0e6cbf6a14
Arthur Reis <arthureisdev@gmail.com>
2023-03-14 19:07:29 -0300

    Foi criada a page Home com uma barra de navegação inferior. Até aqui a barra inferior não esta funcionando devidamente devido o algoritimo. Sem erro no código mas com mal funcionamento. Tive ótima adaptação e facilidade com a linguagem e o Framework até aqui.

--- .meditation_app/lib/main.dart
+++ .meditation_app/lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:meditation_app/constants.dart';
+import 'package:meditation_app/widgets/bottom_nav_bar.dart';
+import 'package:meditation_app/widgets/category_card.dart';
 
 void main() {
   runApp(const MyApp());
@@ -20,6 +22,7 @@ class MyApp extends StatelessWidget {
         scaffoldBackgroundColor: kBackgroundColor,
         textTheme: Theme.of(context).textTheme.apply(displayColor: kTextColor),
       ),
+      // ignore: prefer_const_constructors
       home: HomeScreen(),
     );
   }
@@ -30,47 +33,97 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    var size = MediaQuery.of(context).size;
+    var size = MediaQuery.of(context)
+    .size;
     return Scaffold(
-      body: Stack(children: <Widget>[
+      bottomNavigationBar: BottomNavBar(),
+      body: Stack(
+        children: <Widget>[
         Container(
           height: size.height * .45,
           decoration: BoxDecoration(
-              color: Color(0xFFF5CEB8),
-              image: DecorationImage(
-                alignment: Alignment.centerLeft,
-                image: AssetImage("assets/icons/pilates_gpdb.svg"),
-              )),
+            color: Color(0xFFF5CEB8),
+            image: DecorationImage(
+              alignment: Alignment.centerLeft,
+              image: AssetImage("assets/images/undraw_pilates_gpdb.png"),
+            ),
+          ),
         ),
         SafeArea(
-            child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 20),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: <Widget>[
-              Align(
-                alignment: Alignment.topRight,
-                child: Container(
-                  alignment: Alignment.center,
-                  height: 52,
-                  width: 52,
-                  decoration: BoxDecoration(
-                    color: Color(0xFFF3BEA1),
-                    shape: BoxShape.circle,
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Align(
+                  alignment: Alignment.topRight,
+                  child: Container(
+                    alignment: Alignment.center,
+                    height: 52,
+                    width: 52,
+                    decoration: BoxDecoration(
+                      color: Color(0xFFF2BEA1),
+                      shape: BoxShape.circle,
+                    ),
+                    child: SvgPicture.asset("assets/icons/menu.svg"),
                   ),
-                  child: SvgPicture.asset("assets/icons/menu.svg"),
                 ),
-              ),
-              Text(
-                "Good Morning\nShishir",
-                style: Theme.of(context)
-                    .textTheme
-                    .displayMedium
-                    ?.copyWith(fontWeight: FontWeight.w600),
-              ),
-            ],
+                Text(
+                  "Good Mornign \nShishir",
+                  style: Theme.of(context)
+                      .textTheme
+                      .displaySmall
+                      ?.copyWith(fontWeight: FontWeight.w900),
+                ),
+                Container(
+                  margin: EdgeInsets.symmetric(vertical: 30),
+                  padding: EdgeInsets.symmetric(horizontal: 30, vertical: 5),
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(29.5),
+                  ),
+                  child: TextField(
+                    decoration: InputDecoration(
+                      hintText: "Search",
+                      icon: SvgPicture.asset("assets/icons/search.svg"),
+                      border: InputBorder.none,
+                    ),
+                  ),
+                ),
+                Expanded(
+                  child: GridView.count(
+                    crossAxisCount: 2,
+                    childAspectRatio: .85,
+                    crossAxisSpacing: 20,
+                    mainAxisSpacing: 20,
+                    children: <Widget>[
+                      CategoryCard(
+                        title: "Diet Recommendation",
+                        svgScr: "assets/icons/Hamburger.svg",
+                        press: () {},
+                      ),
+                      CategoryCard(
+                        title: "Kegel Exercises",
+                        svgScr: "assets/icons/Excrecises.svg",
+                        press: () {},
+                      ),
+                      CategoryCard(
+                        title: "Meditation",
+                        svgScr: "assets/icons/Meditation.svg",
+                        press: () {},
+                      ),
+                      CategoryCard(
+                        title: "Yoga",
+                        svgScr: "assets/icons/yoga.svg",
+                        press: () {},
+                      ),
+                    ],
+                  ),
+                )
+              ],
+            ),
           ),
-        )),
+        )
       ]),
     );
   }
